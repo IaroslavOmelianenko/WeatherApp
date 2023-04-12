b5c37dfefbe8cfa083b16da1a4b0acc403a06ea6
IAROSLAV OMELIANENKO <om-y@yandex.ru>
2023-04-12 21:53:27 +0300

    add main fragment with nav

--- app/src/main/java/com/github/iaroslavomelianenko/weatherapp/MainActivity.kt
+++ app/src/main/java/com/github/iaroslavomelianenko/weatherapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.github.iaroslavomelianenko.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.iaroslavomelianenko.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.fragment_main)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
